03de44875a05fd819177c53d2e7b80f6c2c109a8
J <74514216+thecloudybot@users.noreply.github.com>
2021-03-28 22:47:22 -0400

    Update ids.lua

--- u/ids.lua
+++ u/ids.lua
@@ -1,1 +1,10 @@
+127994407  |  beta_millions | hwid = L(6)
+300029997  |  beta_witz | hwid = L(8)
+1451185589 |  beta_5rsm | hwid = L(10)
 
+
+'F7C9F4B5-A830-4FAB-A6EF-F956B7A481D2' -- millions
+
+'DD175122-9789-4EB1-BF33-EF8C35932F45' -- witz
+
+'DB33F229-910A-46FF-8B52-69Y181AT5AB1' -- 5rsm
