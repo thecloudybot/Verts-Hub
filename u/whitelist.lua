f32039dd8b758da222cad4e38deebb288de50337
J <74514216+thecloudybot@users.noreply.github.com>
2021-04-07 14:04:24 -0400

    Update whitelist.lua

--- u/whitelist.lua
+++ u/whitelist.lua
@@ -1,6 +1,6 @@
 127994407  |  beta_millions | hwid = L(6)
-1088978413  |  beta_trolled | hwid = L(8)
-1451185589 |  beta_5rsm | hwid = L(10)
+1088978413 |  beta_trolled  | hwid = L(8)
+1451185589 |  beta_5rsm     | hwid = L(10)
 
 
 'F7C9F4B5-A830-4FAB-A6EF-F956B7A481D2' -- millions
